d72dcebec60b85989c0973521b185fc18493be74
Antyss77 <fragnatiikpvpbest@gmail.com>
2023-02-04 20:08:45 +0100

    Initial Commit

---
+++ BindingPractice/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BindingPractice.ViewModels {
+    public partial class MainWindowViewModel : ObservableObject {
+
+
+        private string greeting = "Hi Prisco";
+
+        public string Greeting {
+            get {
+                return greeting;
+            }
+            set {
+                SetProperty(ref greeting, value);
+
+            }
+        }
+
+        [RelayCommand]
+        private void ChangeDogsName() {
+            if (greeting == "Hi Fido")
+                Greeting = "Hi Prisco";
+            else
+                Greeting = "Hi Fido";
+        }
+    }
+}
